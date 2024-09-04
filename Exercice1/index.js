481b48a03e2c0967a57d82004feeff1042fa4cb9
oliflox <83953551+oliflox@users.noreply.github.com>
2024-09-04 14:01:55 +0200

    Exercice 1

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,11 @@
-//votre code ici
+export const pairNumbers = (min, max) => {
+    const pairs = [];
+    for (let i = min; i <= max; i++) {
+        if (i % 2 === 0) {
+            pairs.push(i);
+        }
+    }
+    return pairs.join(',');
+}
 
-export default pairNumbers
+export default pairNumbers;
