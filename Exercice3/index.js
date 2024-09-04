c776e3f2b5f0f74e1645d3d97f450b5ea2b0ffef
oliflox <83953551+oliflox@users.noreply.github.com>
2024-09-04 14:08:58 +0200

    Exercice 3

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -1,4 +1,10 @@
-// Créez votre fonction ici
+export const calculateAverage = (numbers) => {
+    if (!numbers) {
+        return 'No numbers to calculate average';
+    }
+    const sum = numbers.reduce((acc, number) => acc + number, 0);
+    return sum / numbers.length;
+}
 
 // Exemples d'utilisation de la fonction
 console.log(calculateAverage([5, 10, 15])) // retourne 10
